976a7bc220e9007cb7d21f44679a7ccebee870fa
Lavergnat Jérémie <lavergnatj@outlook.fr>
2026-01-29 15:55:54 +0100

    feat: add Button component stories for Storybook

---
+++ app/components/Button/Button.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+import Button from "./Button";
+
+const meta: Meta<typeof Button> = {
+  title: "Components/Button",
+  component: Button,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+export const Primary: Story = {
+  args: {
+    label: "Click Me",
+    bgFull: true,
+  },
+};
