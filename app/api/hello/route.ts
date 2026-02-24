1a53a56eab5dc0d85e82c09796ee87b694565714
Sabrina <sabrinah_38@hotmail.fr>
2026-02-24 09:32:28 +0100

    chore: setup prettier with eslint integration

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,7 +1,7 @@
-import { prisma } from "@/lib/prisma"
+import { prisma } from "@/lib/prisma";
 
 export async function GET() {
-  const data = await prisma.user.findMany()
+  const data = await prisma.user.findMany();
 
-  return Response.json({ message: 'Hello World', data })
+  return Response.json({ message: "Hello World", data });
 }
