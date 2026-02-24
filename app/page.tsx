8793b226a7f25c4cdecd74532771d86240499dcc
Lavergnat Jérémie <lavergnatj@outlook.fr>
2026-02-24 10:06:48 +0100

    feat: update Button component to support icon props and add MessageSquareIcon

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import styles from "./page.module.scss";
 import Button from "./components/Button/Button";
+import { MessageSquareIcon } from "./components/Icons/Icons";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Button type="tertiary" href="/" label="Home" disabled />
+        <Button
+          type="tertiary"
+          href="/"
+          label="Home"
+          icon={<MessageSquareIcon />}
+          iconPosition="left"
+        />
       </main>
     </div>
   );
