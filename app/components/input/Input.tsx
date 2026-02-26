59638f988cc30c82cbce023aa322512f67812d91
valerieRas <rasolovalerie@gmail.com>
2026-02-26 14:55:27 +0100

    feat: update Input component to use CSS modules for styling

--- app/components/input/Input.tsx
+++ app/components/input/Input.tsx
@@ -1,34 +1,38 @@
-import './Input.module.scss';
+"use client";
+import styles from "./Input.module.scss";
 
-interface InputProps{
-    label?: string;
-    placeholder?: string;
-    value?: string;
-    onChange?: (value: string) => void;
-    disabled?: boolean;
-    type?: string;
-    error?: boolean;
-    errorMessage?: string;
-    obligatory?: boolean;
+interface InputProps {
+  label?: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: (value: string) => void;
+  disabled?: boolean;
+  type?: string;
+  error?: boolean;
+  errorMessage?: string;
+  obligatory?: boolean;
 }
 
-
 export default function Input(props: InputProps) {
-
   return (
-    <div className="inputContainer">
-      {props.label && <label className="inputLabel">{props.label} {props.obligatory && <span className="required">*</span>}</label>}
+    <div className={styles.inputContainer}>
+      {props.label && (
+        <label className={styles.inputLabel}>
+          {props.label}{" "}
+          {props.obligatory && <span className={styles.required}>*</span>}
+        </label>
+      )}
       <input
         type={props.type || "text"}
         placeholder={props.placeholder}
         value={props.value}
         onChange={(e) => props.onChange?.(e.target.value)}
         disabled={props.disabled}
-        className={`inputField ${props.error ? 'errorActive' : ''}`}
+        className={`${styles.inputField} ${props.error ? styles.errorActive : ""}`}
       />
 
       {props.error && props.errorMessage && (
-        <span className="errorMessage">{props.errorMessage}</span>
+        <span className={styles.errorMessage}>{props.errorMessage}</span>
       )}
     </div>
   );
